b6b7e42f31155ff72944640b2e57d07029c57a98
Christian Fiedler <fdlr.christian@gmail.com>
2021-03-05 00:50:20 +0100

    Implement maximize button

--- web/assets/gui.js
+++ web/assets/gui.js
@@ -13,7 +13,8 @@ var gui = {
 	}
 };
 
-function guiDragStartListener(event) {
+function guiDragStartListener(event)
+{
 	if (!event.target.classList.contains('window-titlebar')) {
 		return;
 	}
@@ -33,7 +34,8 @@ function guiDragStartListener(event) {
 	gui.drag.y = gui.drag.startY;
 }
 
-function guiDragEndListener(event) {
+function guiDragEndListener(event)
+{
 	var dragged = gui.drag.elem;
 	if (dragged === null)
 		return;
@@ -41,7 +43,8 @@ function guiDragEndListener(event) {
 }
 
 
-function guiDragOverListener(event) {
+function guiDragOverListener(event)
+{
 	event.preventDefault();
 	
 	var dragged = gui.drag.elem;
@@ -55,7 +58,8 @@ function guiDragOverListener(event) {
 	
 }
 
-function guiLargestZ() {
+function guiLargestZ()
+{
 	var arr = Array.from(document.getElementsByClassName('window')).map(function(elem) {
 		return elem.style.zIndex;
 	});
@@ -64,34 +68,118 @@ function guiLargestZ() {
 	return z;
 }
 
-function guiWindowCloseHandler(event) {
+function guiWindowCloseHandler(event)
+{
 	var win = event.target.parentElement.parentElement;
 	console.log(win);
 	win.parentElement.removeChild(win);
 }
 
-function guiCreateWindow(opts) {
+function removeSaveClasses(elem, cb)
+{
+	Array.from(elem.classList).filter(function(cl) {
+		return cl.startsWith('saved-style-');
+	}).forEach(function(cl) {
+		if(cb) cb(elem, cl);
+		elem.classList.remove(cl);
+	});
+}
+
+function saveSizeAsClasses(elem)
+{
+	removeSaveClasses(elem);
+	['left', 'top', 'width', 'height'].forEach(function(attr) {
+		elem.classList.add('saved-style-'+attr+'-'+btoa(elem.style[attr]));
+	});
+}
+
+function restoreSizeFromClasses(elem)
+{
+	removeSaveClasses(elem, function(elem, cl) {
+		console.log('class: ', cl);
+		var s = cl.substr(12).split('-');
+		var attr = s[0];
+		var value = atob(s[1]);
+		console.log(attr + ' = ' + value);
+
+		elem.style[attr] = value;
+	});
+}
+
+function guiWindowToggleMaximize(win)
+{
+	if (win.classList.contains('maximized')) {
+		restoreSizeFromClasses(win);
+		win.classList.remove('maximized');
+		return;
+	}
+
+	saveSizeAsClasses(win);
+	win.style.top = '0px';
+	win.style.left = '0px';
+	win.style.width = 'calc(100% - 8px)';
+	win.style.height = 'calc(100% - 8px)';
+	win.classList.add('maximized');
+}
+
+function guiCloseButtonMouseDownHandler(event)
+{
+	event.preventDefault();
+	event.target.style.borderStyle = 'inset';
+	gui.drag.elem = null;
+}
+function guiCloseButtonMouseUpHandler(event)
+{
+	event.target.style.borderStyle = 'outset';
+	setTimeout(function() {
+		guiWindowCloseHandler(event)
+	}, 100);
+}
+function guiMaximizeButtonMouseDownHandler(event)
+{
+	event.preventDefault();
+	event.target.style.borderStyle = 'inset';
+	gui.drag.elem = null;
+}
+function guiMaximizeButtonMouseUpHandler(event)
+{
+	event.target.style.borderStyle = 'outset';
+	setTimeout(function() {
+		guiWindowToggleMaximize(event.target.parentElement.parentElement);
+	}, 100);
+}
+
+function guiCreateWindow(opts)
+{
 	var win = document.createElement('div');
 	var tb = document.createElement('div');
 	var title = document.createElement('span');
 	var content = document.createElement('div');
+	var maximizeButton = document.createElement('div');
 	var closeButton = document.createElement('div');
 
 	win.classList.add('window');
 	tb.classList.add('window-titlebar', 'unselectable');
 	content.classList.add('window-content', 'unselectable');
+	maximizeButton.classList.add('window-titlebar-icon', 'unselectable');
 	closeButton.classList.add('window-titlebar-icon', 'unselectable');
    
 	tb.appendChild(title);
 	tb.appendChild(closeButton);
+	tb.appendChild(maximizeButton);
 	tb.draggable = true;
 
 	win.appendChild(tb);
 	win.appendChild(content);
 
 	title.innerText = opts.title || '<unnamed>';
+	maximizeButton.innerText = '⛶';
 	closeButton.innerText = 'x';
-	closeButton.addEventListener('click', guiWindowCloseHandler);
+	//closeButton.addEventListener('click', guiWindowCloseHandler);
+	maximizeButton.addEventListener('mousedown', guiMaximizeButtonMouseDownHandler);
+	maximizeButton.addEventListener('mouseup', guiMaximizeButtonMouseUpHandler);
+	closeButton.addEventListener('mousedown', guiCloseButtonMouseDownHandler);
+	closeButton.addEventListener('mouseup', guiCloseButtonMouseUpHandler);
 
 	win.style.top = (opts.y ? opts.y : 200) + 'px';
 	win.style.left = (opts.x ? opts.x : 200) + 'px';
@@ -114,3 +202,4 @@ function guiInit()
 
 
 guiInit();
+
